d2e72f857359f0bac5cecebd983b1ccb1586feae
Jesse Monfore <monforedesigns@gmail.com>
2020-03-24 22:59:52 -0700

    updated .js

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+
+const overlay = document.getElementsById('overlay');
+let btn__reset = document.querySelector('#overlay');
+btn__reset.addEventListener('click', function(){
+    overlay.style.display = 'none';
+});
+
+
+// return a random phrase from an array
+const getRandomPhraseAsArray = arr => {
+
+}
+
+// adds the letters of a string to the display
+const addPhraseToDisplay = arr => {
+
+}
+
+// check if a letter is in the place
+const checkLetter = button => {
+
+}
+
+// check if the game has been won or lost 
+const checkwin = () => {
+
+}
+
+// listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
+
+}); 
+
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
+
+});
+
+
+
+
